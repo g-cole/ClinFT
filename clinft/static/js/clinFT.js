cb8524d364b340e0a5e24644b461b20434a7a67e
g-cole <g-cole@users.noreply.github.com>
2017-11-01 11:57:43 -0600

    dict obj target list

--- clinft/static/js/clinFT.js
+++ clinft/static/js/clinFT.js
@@ -23,9 +23,10 @@ function handleScroll() {
 function applyHighlights(text) {
     text = text
         .replace(/\n$/g, '\n\n')
-        .replace(/[A-F].*?\b/g, '<mark class="green mark tooltip">$&</mark>')
-        .replace(/[G-P].*?\b/g, '<mark class="blue mark tooltip">$&</mark>')
-        .replace(/[Q-Z].*?\b/g, '<mark class="red mark tooltip">$&</mark>');
+        // .replace(/[A-F].*?\b/g, '<mark class="green mark tooltip">$&</mark>')
+        // .replace(/[G-P].*?\b/g, '<mark class="blue mark tooltip">$&</mark>')
+        // .replace(/[Q-Z].*?\b/g, '<mark class="red mark tooltip">$&</mark>')
+        .replace(re, '<mark class="green mark tooltip">$&</mark>')
     return text;
 }
 
@@ -39,9 +40,13 @@ document.addEventListener('DOMContentLoaded', function(event) {
         clinFTList[i].dispatchEvent(event); //apply highlights to each clinFT instance when page loads
     }
 
+    //Create the tooltip element
     var tt = document.createElement('div');
     tt.id = 'tooltip';
     document.body.appendChild(tt);
+    tt.addEventListener("mouseout", function(){
+        this.style.visibility = "hidden";
+    });
 });
 
 //The highlights are behind the textarea, so chain the hover events - UPDATE THIS FOR IE COMPATIBILITY (IE doesn't like "new Event" inline)!!!
@@ -55,6 +60,10 @@ function processMarks(){
             this.style.display = "none"
             var tar = document.elementFromPoint(ev.clientX, ev.clientY);
             this.style.display = "block";
+            //if (!tar.classList.contains('mark') || !tar.classList.contains('btn')) {
+            if (tar != document.getElementById('tooltip') || tar != document.getElementById('tooltip').getElementsByClassName('btn')[0]) {
+                document.getElementById('tooltip').style.visibility = "hidden";
+            }
             tar.dispatchEvent(new Event('mousemove'));
         },false);
     }
@@ -62,11 +71,20 @@ function processMarks(){
 
 function handleHover(highlight) {
     var theText = highlight.innerHTML;
+    var theTextL = theText.toLowerCase();
     //document.getElementById('examType').value = theText;
     var tooltip = document.getElementById('tooltip')
-    tooltip.innerHTML = theText;
+    tooltip.innerHTML = 'Discovered SNOMED concept:<br>' + theText + ' : ' + snomed[theTextL] + '<button class="btn">Apply SNOMED code</button>';
+    //tooltip.innerHTML += '<button class="btn">Apply SNOMED code</button>';
     hpos = highlight.getBoundingClientRect();
-    tooltip.style.left = hpos.left-70+'px';
-    tooltip.style.top = hpos.top-100+window.scrollY+'px';
-    tooltip.style.visibility = "visible";
+    tooltip.style.left = hpos.left-125+(hpos.width/2)+'px'; //150 = half tooltip width
+    tooltip.style.top = hpos.top-80+window.scrollY+'px'; //80 = tooltip height
+    tooltip.style.visibility = 'visible';
 }
+
+var snomed = {
+    "esophageal varices" : "28670008",
+    "dysphagia" : "40739000",
+    "heartburn" : "16331000"
+};
+var re = new RegExp( Object.keys(snomed).join("|"), "ig");
