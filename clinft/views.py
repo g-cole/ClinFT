4109ee884ac692c80ed629af7936eadd99d95a12
g-cole <g-cole@users.noreply.github.com>
2017-12-11 17:27:47 -0700

    fhir update

--- clinft/views.py
+++ clinft/views.py
@@ -12,5 +12,5 @@ def proc_pick(patient_id):
 @app.route('/<patient_id>/<procedure_id>')
 def proc_doc(patient_id, procedure_id):
 	patient = fhir_tools.get_patient_info(patient_id)
-	procedure = fhir_tools.get_procedure_info('01')
+	procedure = fhir_tools.get_procedure_info(procedure_id)
 	return render_template('index.html', title='Home', patient=patient, procedure=procedure)
--- fhir_tools/fhir_tools.py
+++ fhir_tools/fhir_tools.py
@@ -7,9 +7,9 @@ settings = {
 }
 
 def get_patient_info(pat_id):
-	'''
+	"""
 	words
-	'''
+	"""
 
 	smart = client.FHIRClient(settings=settings)
 	import fhirclient.models.patient as p
@@ -27,7 +27,38 @@ def get_patient_info(pat_id):
 			'gender' : gender}
 
 def get_procedure_info(proc_id):
-	pass
+	"""
+	more words
+	"""
+	smart = client.FHIRClient(settings=settings)
+	import fhirclient.models.procedure as p
+	proc = p.Procedure.read(proc_id, smart.server)
+
+	exam_type = proc.code.coding[0].display
+	indications = proc.reasonCode[0].text
+	conditions = proc.reasonReference#[0].reference.code.coding[0].display
+
+	#import fhirclient.models.condition as c
+	#condition = c.Condition.read('8', smart.server)
+	#condition_text = 'hi'#condition.code.coding[0].display
+
+	#search = c.Condition.where(struct={'id': '8'})
+	#condition1text = condition1.perform_resources(smart.server)
+	#for condition in proc.reasonReference
+
+
+#search = p.Procedure.where(struct={'subject': 'hca-pat-1', 'status': 'completed'})
+#procedures = search.perform_resources(smart.server)
+#for procedure in procedures:
+#    procedure.as_json()
+#    # {'status': u'completed', 'code': {'text': u'Lumpectomy w/ SN', ...
+
+
+	
+	return {'exam_type' : exam_type,
+			'indications' : indications,
+			'condition' : conditions
+			}
 
 def calculate_age(born):
     today = date.today()
